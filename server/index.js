11f202cb55bc7a1d284bfe4d833e5dc6a151c813
Naruebodin-Aetal <naruebodin.a@ku.th>
2025-03-09 12:51:12 +0700

    validateData

--- server/index.js
+++ server/index.js
@@ -23,6 +23,29 @@ const initMySQL = async () => {
     })
 }
 
+const validateData = (userData) => {
+    let errors = []
+    if(!userData.firstName){
+        errors.push ('กรุณากรอกชื่อ')
+    }
+    if(!userData.lastName){
+        errors.push ('กรุณากรอกนามสกุล')
+    }
+    if(!userData.age){
+        errors.push ('กรุณากรอกอายุ')
+    }
+    if(!userData.gender){
+        errors.push ('กรุณาเลือกเพศ')
+    }
+    if(!userData.interests){
+        errors.push ('กรุณาเลือกสิ่งที่สนใจ')
+    }
+    if(!userData.description){
+        errors.push ('กรุณากรอกคำอธิบาย')
+    }
+    return errors
+}
+
 
 /*
 app.get('/testdbnew', async (req, res) => {
@@ -50,16 +73,25 @@ app.post('/users', async (req, res) => {
    
     try{
         let user = req.body;
+        const errors = validateData(user)
+        if(errors.length > 0){
+            throw{
+                message: 'กรุณากรอกข้อมูลให้ครบถ้วน',
+                errors: errors
+            }
+        }
         const results = await conn.query('INSERT INTO users SET ?', user)
         res.json({
             message: 'Create user successfully',
             data: results[0]
         })
     }catch(error){
+        const errorMessages = error.message || 'something went wrong'
+        const errors = error.errors || []
         console.error('error: ', error.message)
         res.status(500).json({
-            message: 'something went wrong',
-            errorMessage: error.message
+            message: errorMessages,
+            errors: errors
         })
     }
 })
